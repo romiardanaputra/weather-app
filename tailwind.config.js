8fc0175f96cedc19740301e4c94fd7f920f6571d
Kadek Romi Ardana Putra <romiardana21@gmail.com>
2024-01-09 14:19:27 +0800

    update index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,css}", "./public/*"],
-  theme: {
-    extend: {
-      colors: {
-        text: '#eaeaea',
-        background: '#1E2E4A',
-        primary: '#155831',
-        secondary: '#d7e4dc',
-        accent: '#A3B0C7',
-        primary2: '#172338'
-       },
-    },
-  },
-  plugins: [],
-}
+   content: ["./src/**/*.{html,js,css}", "./public/*"],
+   theme: {
+      extend: {
+         colors: {
+            text: "#eaeaea",
+            background: "#1E2E4A",
+            primary: "#155831",
+            secondary: "#d7e4dc",
+            accent: "#A3B0C7",
+            primary2: "#172338",
+         },
+      },
+   },
+   plugins: ['prettier-plugin-tailwindcss'],
+};
